a49e9e098b95f418e731086c86c81cdc5fc91752
DaryaVeretennikova <darya0veretennikova@gmail.com>
2017-08-20 00:08:36 +0300

    refactoring

---
+++ README.md
@@ -0,0 +1,7 @@
+# Тестовое задание для Школы Node.JS
+
+### Возможные значения для атрибута формы action 
+
+- https://api.github.com/repos/DaryaVeretennikova/yandex-node-school/contents/json/success.json
+- https://api.github.com/repos/DaryaVeretennikova/yandex-node-school/contents/json/error.json
+- https://api.github.com/repos/DaryaVeretennikova/yandex-node-school/contents/json/progress.json
--- index.js
+++ index.js
@@ -1,44 +1,43 @@
+let NyForm;
+
 window.onload = () => {
   const inputsNames = ['fio', 'email', 'phone'];
   const form = document.getElementById('myForm');
+  const submit = form.querySelector('input[type="submit"]');
   const resultContainer = document.getElementById('resultContainer');
 
-  const MyForm = {
+  const fioRegexp = /^([A-Za-zА-Яа-яёЁ]+\s+){2}[A-Za-zА-Яа-яёЁ]+\s*$/;
+  const phoneRegexp = /^\+7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}$/;
+  const emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))\@{1}(ya\.ru|yandex\.ru|yandex\.ua|yandex\.by|yandex\.kz|yandex\.com){1}$/i;
+
+  const checkSum = phone => {
+    const sum = phone
+      .match(/\d+/g)
+      .join('')
+      .split('')
+      .reduce((sum, current) => sum + parseInt(current), 0);
+
+    return sum <= 30;
+  };
+
+  MyForm = {
     validate: function() {
-      const validEmails = [
-        'ya.ru',
-        'yandex.ru',
-        'yandex.ua',
-        'yandex.by',
-        'yandex.kz',
-        'yandex.com'
-      ];
       let isValid;
       let ErrorFields = [];
 
-      const setValid = (input, inputName) => {
-        input.classList.remove('error');
-        ErrorFields = ErrorFields.filter(item => item !== inputName);
-      };
-
-      const setInvalid = (input, inputName) => {
-        input.classList.add('error');
-        ErrorFields.push(inputName);
-      };
-
-      const phoneRegexp = /^\+7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}$/;
-      const checkSum = phone => {
-        const sum = phone
-          .split('')
-          .filter(item => Number.isInteger(parseInt(item)))
-          .reduce((sum, current) => sum + parseInt(current), 0);
-
-        return sum <= 30;
-      };
-
       inputsNames.forEach(inputName => {
-        const input = document.querySelector(`input[name="${inputName}"]`);
+        const input = form.querySelector(`input[name="${inputName}"]`);
         const value = input.value.trim();
+
+        const setValid = (input, inputName) => {
+          input.classList.remove('error');
+          ErrorFields = ErrorFields.filter(item => item !== inputName);
+        };
+
+        const setInvalid = (input, inputName) => {
+          input.classList.add('error');
+          ErrorFields.push(inputName);
+        };
 
         if (value.length === 0) {
           setInvalid(input, inputName);
@@ -46,17 +45,13 @@ window.onload = () => {
         }
 
         if (inputName === 'fio') {
-          if (value.split(' ').length === 3) {
+          if (fioRegexp.test(value)) {
             setValid(input, inputName);
           } else {
             setInvalid(input, inputName);
           }
         } else if (inputName === 'email') {
-          const emailDomain = value.match(/@(.*)$/)[0];
-
-          if (
-            validEmails.filter(item => `@${item}` === emailDomain).length === 1
-          ) {
+          if (emailRegexp.test(value)) {
             setValid(input, inputName);
           } else {
             setInvalid(input, inputName);
@@ -83,7 +78,7 @@ window.onload = () => {
       const data = {};
 
       inputsNames.forEach(inputName => {
-        const input = document.querySelector(`input[name="${inputName}"]`);
+        const input = form.querySelector(`input[name="${inputName}"]`);
         data[inputName] = input.value;
       });
 
@@ -92,31 +87,44 @@ window.onload = () => {
 
     setData: function(data) {
       inputsNames.forEach(inputName => {
-        const input = document.querySelector(`input[name="${inputName}"]`);
+        const input = form.querySelector(`input[name="${inputName}"]`);
         input.value = data[inputName];
       });
     },
 
     submit: function() {
       if (this.validate().isValid) {
-          const url = encodeURI(form.getAttribute('action'));
+        const url = encodeURI(form.getAttribute('action'));
 
-          fetch(url, { method: 'post', body: new FormData(form), mode: 'no-cors' })
-            .then((resp) => {
-                if (resp.status === 'success') {
-                    resultContainer.classList.add('success');
-                    resultContainer.innerHTML = 'Success';
-                } else if (resp.status === 'error') {
-                    resultContainer.classList.add('error');
-                    resultContainer.innerHTML = resp.reason;
-                } else if (resp.status === 'progress') {
-                    resultContainer.classList.add('progress');
+        submit.setAttribute('disabled', 'disabled');
 
-                    setInterval(() => {
-                        this.submit();
-                    }, resp.timeout);
-                }
-            });
+        fetch(url).then(response => {
+          response.json().then(json => {
+            let resp;
+
+            try {
+              resp = JSON.parse(atob(json.content));
+            } catch (error) {
+              return;
+            }
+
+            if (resp.status === 'success') {
+              resultContainer.classList.add('success');
+              resultContainer.innerHTML = 'Success';
+              submit.removeAttribute('disabled');
+            } else if (resp.status === 'error') {
+              resultContainer.classList.add('error');
+              resultContainer.innerHTML = resp.reason;
+              submit.removeAttribute('disabled');
+            } else if (resp.status === 'progress') {
+              resultContainer.classList.add('progress');
+
+              setInterval(() => {
+                this.submit();
+              }, resp.timeout);
+            }
+          });
+        });
       }
     }
   };
